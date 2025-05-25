a6b025859314f35d24057e481a405d98fb3f9aa0
Thai Mai <jakehauer1234@gmail.com>
2025-05-25 10:08:02 +0700

    init the repo

---
+++ NewsManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewsManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
